7277b3b71e419f2b2b43e01fec5392f491e004de
Marcelo Barbosa <barbosa.marcelo6@gmail.com>
2021-02-20 14:42:20 -0300

    Validar usuario existente

--- js/app.js
+++ js/app.js
@@ -8,34 +8,61 @@ $(function () {
 
   function loadClient() {
     gapi.client.setApiKey("AIzaSyDPqQmiTFpu8lwC7T7nJwEJduk-X-bO0bc");
+
     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
       .then(function () {
-        console.log("GAPI client loaded for API");
-        var videoId = $('input[name="live_id"]').val();
+        getLives();
+      }, function (err) { console.error("Error loading GAPI client for API", err); });
+  }
 
-        return gapi.client.youtube.liveBroadcasts.list({
-          "part": [
-            "snippet,contentDetails,status"
-          ],
-          "id": [ videoId ]
-        }).then(function (response) {
-            console.log("Response", response);
-        }, function (err) { console.error("Execute error", err); });
-      },
-        function (err) { console.error("Error loading GAPI client for API", err); });
+  function getLives() {
+    var videoId = $('input[name="live_id"]').val();
+
+    return gapi.client.youtube.liveBroadcasts.list({
+      "part": [
+        "snippet,contentDetails,status"
+      ],
+      "id": [videoId]
+    }).then(function (response) {
+      var lives = response.result.items;
+
+      if (lives.length > 0) {
+        var liveId = lives[0].snippet.liveChatId;
+        getComments(liveId);
+      }
+    }, function (err) { console.error("Execute error", err); });
+  }
+
+  function getComments(liveId) {
+    return gapi.client.youtube.liveChatMessages.list({
+      "part": [
+        "snippet,authorDetails"
+      ],
+      "liveChatId": liveId,
+      "maxResults": 10
+    }).then(function (response) {
+      var lives = response.result.items;
+
+      if (lives.length > 0) {
+        var liveId = lives[0].snippet.liveChatId;
+        getComments(liveId);
+      }
+    }, function (err) { console.error("Execute error", err); });
   }
 
   function execute() {
-    authenticate().then(loadClient);
+    if (!gapi.auth2.isSignedIn.get()) return authenticate().then(loadClient);
+
+    getLives();
   }
 
   gapi.load("client:auth2", function () {
-    gapi.auth2.init({
+    auth2 = gapi.auth2.init({
       client_id: "1071083884583-n46pbsol5s9q215o52h45tr7o1lih2kj.apps.googleusercontent.com"
     });
   });
 
-  $('button').on('click', function(){
+  $('button').on('click', function () {
     execute();
   });
 });
