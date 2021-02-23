4689fa98351cb2b00a84a8effff2511fa29fb1b4
Marcelo Barbosa <barbosa.marcelo6@gmail.com>
2021-02-23 10:19:40 -0300

    Mostrar mensagem recebida

--- js/app_preview.js
+++ js/app_preview.js
@@ -4,7 +4,7 @@ $(function () {
     console.log(socket.handshake.query); // prints { x: "42", EIO: "4", transport: "polling" }
   });
 
-  socket.on("selected_message", (object) => {
+  socket.on("message_selected_by_host", (object) => {
     console.log("MSG:: ",object);
   });
 });
