7a5caa4e566951b9a187782f35773ff49e03a22b
Marcelo Barbosa <barbosa.marcelo6@gmail.com>
2021-02-23 10:25:36 -0300

    Mostrar mensagem ativa

--- js/app_preview.js
+++ js/app_preview.js
@@ -5,6 +5,23 @@ $(function () {
   });
 
   socket.on("message_selected_by_host", (object) => {
-    console.log("MSG:: ",object);
+    if (object) {
+      var id = object.id;
+      var channel = object.authorDetails;
+      var message = object.snippet.displayMessage;
+
+      var html = [
+        '<li data-id="' + id + '">',
+        '<img src="' + channel.profileImageUrl + '"/>',
+        '<div>',
+        '<h4>' + channel.displayName + '</h4>',
+        '<p>' + message + '</p>',
+        '</div>',
+        '</li>'
+      ];
+      $('#comments .messages ul').html(html.join(''));
+    } else {
+      $('#comments .messages ul').html('<li><span>Selecione uma mensagem abaixo pra ficar em destaque</span></li>');
+    }
   });
 });
