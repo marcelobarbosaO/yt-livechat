91e959cbbafe4e59f0ac6c5a030f51f8120e3bc3
Marcelo Barbosa <barbosa.marcelo6@gmail.com>
2021-02-23 10:21:55 -0300

    CONECTAR

--- js/app_preview.js
+++ js/app_preview.js
@@ -1,7 +1,7 @@
 $(function () {
   const socket = io("https://cnt-lives.herokuapp.com/");
   socket.on("connection", (socket) => {
-    console.log(socket.handshake.query); // prints { x: "42", EIO: "4", transport: "polling" }
+    console.log('CONNECTED', socket); // prints { x: "42", EIO: "4", transport: "polling" }
   });
 
   socket.on("message_selected_by_host", (object) => {
